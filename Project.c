84459841801a29cd4ac9aef1650a555861f8f145
ntsachira <sct20090@std.uwu.ac.lk>
2022-07-20 12:57:58 +0530

    Update Project.c
    updated

--- Project.c
+++ Project.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-int counter1,counter2, temp1[20];
+int counter1,counter2;
+float temp1[20], tHours[20];
 
 int main()
 {
-    printf("=================================================");
-    printf("\n\tWelcome to the study plan generator");
-    printf("\n=================================================");
+    printf("==================================================================");
+    printf("\n\t\tWelcome to the study plan generator");
+    printf("\n==================================================================");
 
     int modTotal;   //holds the number of modules
 
@@ -17,12 +18,16 @@ int main()
 
     recorder(&mods[0], modTotal); //calling the recorder function
 
-    for(counter1=0; counter1<modTotal; counter1++)
-    {
-        printf("\nModule %d : %s\n",counter1+1,mods[counter1]);
-    }
+
 
     return 0;
+}
+
+
+void divider()
+{
+   // printf("\n******************************************************************");
+    printf("____________________________________________________________________\n\n");
 }
 
 
@@ -34,13 +39,25 @@ void recorder(char *mods, int modTotal)
         scanf("%s",mods);
 
         printf("\nEnter the number of lecture recordings\t: ");
-        scanf("%d",&temp1[0]);
+        scanf("%f",&temp1[0]);
 
         printf("\nEnter the average length of a recording\t: ");
-        scanf("%d",&temp1[1]);
+        scanf("%f",&temp1[1]);
+
+        printf("\nEnter the total number of pages of lecture notes\t: ");
+        scanf("%f",&temp1[2]);
+
+        printf("\nEnter the average time do you need to read 10 pages (in hours)\t: ");
+        scanf("%f",&temp1[3]);
+
+        tHours[counter1]= (temp1[0]*temp1[1])+(temp1[2]*temp1[3]) ;
+
+        temp1[4]+= tHours[counter1]; //temp1[4] = sum of the total hours per module
+
 
         mods+=20;
     }
+divider();
 }
 
 
