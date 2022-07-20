ee7461ddabd631468d6fe30f28d4b2b78f4bf924
ntsachira <sct20090@std.uwu.ac.lk>
2022-07-20 09:35:47 +0530

    Update Project.c
    created a separate function to get records

--- Project.c
+++ Project.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+int counter1,counter2, temp1[20];
 
 int main()
 {
@@ -13,24 +14,54 @@ int main()
     scanf("%d",&modTotal);
 
     char mods[modTotal][20];
-    int counter, temp1[20];
 
+    recorder(&mods[0], modTotal); //calling the recorder function
 
-    for(counter = 0; counter < modTotal; counter++)
+    for(counter1=0; counter1<modTotal; counter1++)
     {
-        printf("\n\nEnter module %d\t: ",counter+1);
-        scanf("%s",&mods[counter]);
+        printf("\nModule %d : %s\n",counter1+1,mods[counter1]);
+    }
+
+    return 0;
+}
+
+
+void recorder(char *mods, int modTotal)
+{
+    for(counter1 = 0; counter1 < modTotal; counter1++)
+    {
+        printf("\n\nEnter module %d\t: ",counter1+1);
+        scanf("%s",mods);
 
         printf("\nEnter the number of lecture recordings\t: ");
         scanf("%d",&temp1[0]);
 
-       // printf("\nEnter the average length of a recording\t: ");
+        printf("\nEnter the average length of a recording\t: ");
         scanf("%d",&temp1[1]);
 
+        mods+=20;
     }
+}
 
 
-    return 0;
+void ranker(int *ptr, int modCount)
+{
+    for(counter1 = 1; counter1 < modCount; counter1++)
+    {
+        for(counter2 = 1; counter2 < modCount; counter2++)
+        {
+            if(*ptr < *(ptr+1))
+            {
+                int c;
+
+                c = *ptr;
+                *ptr = *(ptr+1);
+                *(ptr+1) = c;
+            }
+            ptr++;
+        }
+        ptr = ptr - (modCount-1);
+    }
 }
 
 
